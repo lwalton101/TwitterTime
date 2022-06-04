837abad3b3a3a158e0245a2670b73375440f63f2
lwalton101 <waltonfamily01@outlook.com>
2022-06-04 14:30:07 +0100

    added process env port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 const hbs = require("hbs");
 const exhbs = require("express-handlebars");
 
